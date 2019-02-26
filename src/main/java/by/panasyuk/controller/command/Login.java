3dab883dfd7a7eb7a86d19359be62208d865851b
DenisPan007 <denispan007@gmail.com>
2019-02-26 16:23:36 +0300

    fix login (add hash for password)

--- src/main/java/by/panasyuk/controller/command/Login.java
+++ src/main/java/by/panasyuk/controller/command/Login.java
@@ -4,14 +4,30 @@ import by.panasyuk.service.UserService;
 import by.panasyuk.service.exception.ServiceException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Login implements Command {
+    public static String passwordHash(String password) throws NoSuchAlgorithmException{
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        byte[] passwordByte = sha256.digest(password.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0;i < passwordByte.length; i++){
+            String s = Integer.toHexString(passwordByte[i]&0xff);
+            if(s.length() == 1){
+                s = "0" + s;
+            }
+            sb.append(s);
+        }
+        return sb.toString();
+    }
     @Override
     public String execute(HttpServletRequest req) throws CommandException {
         UserService service = UserService.getInstance();
         String login = req.getParameter("login");
         String password = req.getParameter("password");
         try {
+            password = passwordHash(password);
             if (!service.login(login,password)){
                // req.setAttribute("invalidLogin",true);
                 return "/WEB-INF/views/login.jsp";
@@ -20,7 +36,7 @@ public class Login implements Command {
                 req.setAttribute("login",login);
                 return "/WEB-INF/views/successfulLogin.jsp";
             }
-        }catch (ServiceException e){
+        }catch (ServiceException | NoSuchAlgorithmException e){
             throw new CommandException(e);
         }
 
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -21,7 +21,8 @@
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-
+<h1>Login: admin</h1>
+    <h1>Password: admin</h1>
 </main>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
