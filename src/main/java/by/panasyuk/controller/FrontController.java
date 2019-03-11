618cd9dcba1908c7655e26c85a88b232a6da7130
DenisPan007 <denispan007@gmail.com>
2019-03-11 16:12:21 +0300

    fix connection pool(add checking connection for validity)

--- src/main/java/by/panasyuk/controller/FrontController.java
+++ src/main/java/by/panasyuk/controller/FrontController.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 public class FrontController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //processRequest(request, response);
         processRequest(req, resp);
     }
 
